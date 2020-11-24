53dcb8c1911164a2feec6da6ab2126190788fdb7
huchiwen <clot48@gmail.com>
2020-11-24 23:57:39 +0800

    modify ai.py file

--- ai.py
+++ ai.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
+"""
+author: huchiwen
+language :python3
 
-def check_list_equals(list1,list2):
-
-    num1 = len(list1)
-    num2 = len(list2)
-
-    if num1 == num2:
-        return True
-    return False
+"""
 
 def main():
-
     A = [1,2,3]
-    B = [1,2,3]
+    B = [1,89,3]
     res =0
 
-    result = check_list_equals(A,B) 
-
+    result  = (len(A) == len(B)) 
     if result:
       for i in range(0,len(A)):
           res+=A[i]*B[i]

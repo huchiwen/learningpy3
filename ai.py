9de9c1efeacedcc63a4f069285e9d00bacc67a7c
huchiwen <clot48@gmail.com>
2020-11-24 18:24:49 +0800

    update ai.py file

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def check_list_equals(list1,list2):
+
     num1 = len(list1)
     num2 = len(list2)
 
@@ -13,14 +14,15 @@ def main():
     A = [1,2,3,3]
     B = [1,2,3]
     res =0
+
     result = check_list_equals(A,B) 
+
     if result:
       for i in range(0,len(A)):
           res+=A[i]*B[i]
       print(res)
     else:
       print("list not equal")
-
 
 if __name__ == "__main__":
     main()
